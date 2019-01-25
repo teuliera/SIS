ecef94332a0b2a8997eaf7932c763a77c7fa5abe
Teulieres <anneliseteulieres17@gmail.com>
2019-01-25 10:35:49 +0100

    Test

--- DomTest.java
+++ DomTest.java
@@ -7,6 +7,7 @@ import org.w3c.dom.NodeList;
  * @author Emmanuel Promayon, (c) UJF - Polytech'Grenoble - 2011
  */
 public class DomTest {
+    //ABJJBUG
 
     public static void main(String[] args) {
         String nomDocumentCentre = "src/centre.xml";
